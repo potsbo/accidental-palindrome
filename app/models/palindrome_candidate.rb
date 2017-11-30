532590df256ad924135ce10b2b1137ae0e563974
Shimpei Otsubo <potsbo@gmail.com>
2017-12-01 01:21:08 +0900

    Add weak test

--- app/models/palindrome_candidate.rb
+++ app/models/palindrome_candidate.rb
@@ -1,3 +1,10 @@
 class PalindromeCandidate < ApplicationRecord
-  
+  def initialize(param)
+    param ||= {}
+    @text = param[:text]
+  end
+
+  def longest_palindrome
+    @text
+  end
 end
--- spec/models/palindrome_candidate_spec.rb
+++ spec/models/palindrome_candidate_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe PalindromeCandidate do
 
   let(:text) { 'なんか文章' }
+  let(:cand) { PalindromeCandidate.new(text: text) }
 
   describe '#longest_palindrome' do
-    let(:res) { PalindromeCandidate }
+    let(:res) { cand.longest_palindrome }
     it 'should return a String' do
       expect(res).to be_a String
     end
