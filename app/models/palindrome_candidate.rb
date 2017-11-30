9cbf02e93ca91f4e4887461fc8e3c40c2e78eccd
Shimpei Otsubo <potsbo@gmail.com>
2017-12-01 04:09:21 +0900

    Organize

--- app/models/palindrome_candidate.rb
+++ app/models/palindrome_candidate.rb
@@ -9,13 +9,7 @@ class PalindromeCandidate
     @yomis  = yomis
   end
 
-  def valid?
-    first = @yomis.first
-    return false if INVALID_PARTS.include? first[:part]
-    return false if INVALID_CHARS.include?(first[:surface])
-    true
-  end
-
+  # attributes
   def pronounce
     @pronounce ||= @yomis.
       reject{|y| INVALID_PARTS.include? y[:part]}.
@@ -24,17 +18,6 @@ class PalindromeCandidate
 
   def surface
     @surface ||= @yomis.map{|y| y[:surface]}.join
-  end
-
-  def is_palindrome?
-    return @is_palindrome unless @is_palindrome.nil?
-    return @is_palindrome = false unless valid?
-    str = pronounce
-    return @is_palindrome = false if str.size <= 1
-    for num in 1..str.size/2 do
-      return @is_palindrome = false if str[num-1] != str[str.size-num]
-    end
-    @is_palindrome = true
   end
 
   def summary
@@ -48,10 +31,31 @@ class PalindromeCandidate
     }
   end
 
+
   def <=>(other)
     pronounce.size - other.pronounce.size
     # size_def = pronounce.size - other.pronounce.size
     # return size_def unless size_def.zero?
     # surface.size - other.surface.size
+  end
+
+  private
+
+  def valid?
+    first = @yomis.first
+    return false if INVALID_PARTS.include? first[:part]
+    return false if INVALID_CHARS.include?(first[:surface])
+    true
+  end
+
+  def is_palindrome?
+    return @is_palindrome unless @is_palindrome.nil?
+    return @is_palindrome = false unless valid?
+    str = pronounce
+    return @is_palindrome = false if str.size <= 1
+    for num in 1..str.size/2 do
+      return @is_palindrome = false if str[num-1] != str[str.size-num]
+    end
+    @is_palindrome = true
   end
 end
