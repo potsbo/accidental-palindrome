69bc3f401d09a990a42b45c29d12cd724da5559e
Shimpei Otsubo <potsbo@gmail.com>
2017-12-01 10:47:21 +0900

    Style

--- public/script.js
+++ public/script.js
@@ -2,18 +2,18 @@ var typingTimer;                //timer identifier
 var doneTypingInterval = 100;  //time in ms, 5 second for example
 var $input = $('#query');
 
-function doneTyping () {
+function doneTyping() {
   $.post("https://accidental-palindrome.herokuapp.com/find", { // TODO: make this configuable
     text: $('#query').val()
   }, function(data) {
     $("#text").html(data.text);
-    $("#longest").html(data.longest_palindrome.surface || 'ー')
-    $("#pronounce").html(data.longest_palindrome.pronounce || 'ー')
-    $("#length").html(data.longest_palindrome.size)
+    $("#longest").html(data.longest_palindrome.surface || 'ー');
+    $("#pronounce").html(data.longest_palindrome.pronounce || 'ー');
+    $("#length").html(data.longest_palindrome.size);
   });
 }
 
-doneTyping()
+doneTyping();
 
 $input.on('keyup', function () {
   clearTimeout(typingTimer);
