0d696883e51f1d39951d695dcc6bc1ba61a7f2ab
Shimpei Otsubo <potsbo@gmail.com>
2017-12-01 11:03:31 +0900

    Rename

--- spec/models/palindrome_finder_spec.rb
+++ spec/models/palindrome_finder_spec.rb
@@ -34,7 +34,7 @@ describe PalindromeFinder do
 
   describe '#longest_palindrome' do
     let(:res) { cand.longest_palindrome }
-    it 'should return a String' do
+    it 'should return a PalindromeCandidate' do
       expect(res).to be_a PalindromeCandidate
     end
 
